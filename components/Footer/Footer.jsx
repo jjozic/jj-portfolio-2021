c42a0c7480f741f8c74da1c8679ae0b7bc10e88f
Julian Jozic <jjozic98@gmail.com>
2021-05-13 21:16:38 +0200

    added footer

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Wrapper } from './Styles';
+
+const Footer = () => {
+    return (
+        <Wrapper>
+            jjozic98@gmail.com - instagram - github
+        </Wrapper>
+    )
+}
+
+export default Footer
---
+++ components/Footer/Styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.footer`
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    padding: 0 2rem;
+    background-color: var(--light);
+`

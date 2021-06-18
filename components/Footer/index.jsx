3d58ff7f48e108435c8a2e1f0ecf22db80e6cfd9
Julian Jozic <jjozic98@gmail.com>
2021-06-18 23:49:54 +0200

    added icons and styling to the footer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,19 +1,21 @@
-import ExternalLink from "../../helpers/ExternalLink";
-import styles from '@/styles/Footer.module.scss';
+import { FiGithub, FiInstagram, FiMail } from "react-icons/fi";
+
+import ExternalLink from "@/helpers/ExternalLink";
+import styles from "@/styles/Footer.module.scss";
 
 const Footer = () => {
   return (
     <footer className={styles.wrapper}>
       <div className={styles.links}>
-        <ExternalLink
-          link="mailto:jjozic98@gmail.com"
-          name="jjozic98@gmail.com"
-        />
-        <ExternalLink
-          link="https://instagram.com/julianjozic"
-          name="Instagram"
-        />
-        <ExternalLink link="https://github.com/jjozic" name="Github" />
+        <ExternalLink link="mailto:jjozic98@gmail.com">
+          <FiMail className={styles.icon}/>
+        </ExternalLink>
+        <ExternalLink link="https://instagram.com/julianjozic">
+          <FiInstagram className={styles.icon}/>
+        </ExternalLink>
+        <ExternalLink link="https://github.com/jjozic">
+          <FiGithub className={styles.icon}/>
+        </ExternalLink>
       </div>
     </footer>
   );

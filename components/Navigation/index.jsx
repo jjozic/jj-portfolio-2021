4ce1217f904c06cefd449a6b33033caa4c7387e9
Julian Jozic <jjozic98@gmail.com>
2021-06-18 23:39:47 +0200

    fixed styling and layout for navigation

--- components/Navigation/index.jsx
+++ components/Navigation/index.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import Link from "next/link";
 
-import styles from '@/styles/Navigation.module.scss'
+import styles from "@/styles/Navigation.module.scss";
 
 const Navigation = () => {
   return (
     <nav className={styles.navElement}>
       <div className={styles.wrapper}>
-        <div>
-          <Link href="#projects">Projects</Link>
-          {/* <Link href="#resume">Resumé</Link> */}
-          <Link href="#contact">Contact Me</Link>
-        </div>
-        <div className={styles.logo}>JJ</div>
-        <div>
-          <Link href="#projects">Language</Link>
-          <Link href="#resume">🌛</Link>
-        </div>
+        <ul>
+          <li>
+            <Link href="#projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="#contact">Contact Me</Link>
+          </li>
+          <li className={styles.logoLi}>
+            <div className={styles.logo}>JJ</div>
+          </li>
+          <li>
+            <Link href="#projects">Language</Link>
+          </li>
+          <li>
+            <Link href="#resume">Dark Mode</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );

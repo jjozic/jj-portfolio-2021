b73b2d310cac2f5872a9b77018bb7fd42728b57a
Julian Jozic <jjozic98@gmail.com>
2021-06-19 02:43:20 +0200

    added styling to a links on intro text

--- components/PageContent/index.jsx
+++ components/PageContent/index.jsx
@@ -1,3 +1,5 @@
+import { FiGithub, FiMail } from "react-icons/fi";
+
 import styles from "@/styles/PageContent.module.scss";
 import ExternalLink from "helpers/ExternalLink";
 
@@ -11,8 +13,8 @@ const PageContent = () => {
           with the backend if I need to!
         </p>
         <div className={styles.links}>
-          <ExternalLink link="mailto:jjozic98@gmail.com">Contact Me</ExternalLink>
-          <ExternalLink link="https://github.com/jjozic">Github</ExternalLink>
+          <ExternalLink link="mailto:jjozic98@gmail.com"><FiMail /> Contact Me</ExternalLink>
+          <ExternalLink link="https://github.com/jjozic"><FiGithub /> Github</ExternalLink>
         </div>
       </div>
     </>

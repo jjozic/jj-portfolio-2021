6954fd5dd9b05b86a4d48b292d2e428d1233cef4
Julian Jozic <jjozic98@gmail.com>
2021-06-22 20:09:04 +0200

    added global style for heading

--- components/Projects/index.jsx
+++ components/Projects/index.jsx
@@ -1,16 +1,16 @@
-import styles from '@/styles/Projects.module.scss'
+import styles from "@/styles/Projects.module.scss";
 
 export default function Projects() {
-    return (
-        <div>
-            <h2>Personal Projects</h2>
-            <div className={styles.wrapper}>
-                <article>Project1</article>
-                <article>Project2</article>
-                <article>Project3</article>
-                <article>Project4</article>
-                <article>Project5</article>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h2 className="heading">Personal Projects</h2>
+      <div className={styles.wrapper}>
+        <article>Project1</article>
+        <article>Project2</article>
+        <article>Project3</article>
+        <article>Project4</article>
+        <article>Project5</article>
+      </div>
+    </div>
+  );
 }
